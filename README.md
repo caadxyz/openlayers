45c0bdee1fa2594d95c2f79e5e3728c47f0e59a0
ma haidong <13501108114@163.com>
2015-10-07 14:32:37 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# openlayers
-geoserver+openlayers
+# openlayers 3
+geoserver + openlayers 3  
+
+**tutorial**
+http://openlayers.org/en/v3.9.0/doc/tutorials/
---
+++ base.js
@@ -0,0 +1,4 @@
+var GEOSERVERBASE = "http://localhost:8080";
+var CountyLayer = 'topp:states';
+var CountriesLayer = 'tiger:tiger_roads';
+var RiversLayer = 'tiger:poi';
